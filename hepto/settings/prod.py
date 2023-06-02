ec2ea97225403036cb448e5069ed59b000d01efd
Dennis Kamau Ngera <ngerakam123@gmail.com>
2023-06-02 12:51:48 +0000

    cleanup for prod

--- hepto/settings/prod.py
+++ hepto/settings/prod.py
@@ -2,4 +2,17 @@ from .base import *
 
 
 
+
 ALLOWED_HOSTS = ['*']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hepto',
+        'USER': 'heptodba',
+        'PASSWORD': os.getenv("DATABASE"),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
